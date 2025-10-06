c4f5094d5c99fc73f45ec5418efd25fb562e1886
CodeSmithMayank <mayankpal4812@gmail.com>
2025-10-06 19:18:52 +0530

    Lecture - (  Kadane's Algorithm  ) ( Completed )

--- Lecture - (  Kadane's Algorithm  )/Kadane's_Algorithm.cpp
+++ Lecture - (  Kadane's Algorithm  )/Kadane's_Algorithm.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<limits.h>
 #include<vector>
 using namespace std;
 
@@ -41,11 +42,35 @@ int Maximum_Subarray(vector<int> & nums)
 }
 
 
+//Optimised approach using kadane's algorithm
+int Maximum_Subarray_Using_Kadanes(vector<int> vec)
+{
+    int current_sum = 0 ;
+    int max_sum = INT_MIN;
+
+    for(int i = 0 ; i < vec.size() ; i++)
+    {
+        current_sum+=vec[i]; 
+        
+        //we are finding max over here because of edge case let suppose an array contains all negative number , then if cs become 0 then we will not get that negative number
+        max_sum = max(current_sum,max_sum);
+        if(current_sum<0)
+        {
+            current_sum = 0 ;
+        }
+    }
+
+    return max_sum;
+}
+
 int main()
 {
     //For an array there would be no of subarrays = (n*(n+1))/2
     vector<int> vec{3,-4,5,4,-1,7,-8} ;
     int result = Maximum_Subarray(vec) ;
-    cout << result << endl;
+    cout << "Without Kadane's : " << result << endl;
+
+    int res = Maximum_Subarray_Using_Kadanes(vec);
+    cout << "Using Kadanes's : " << res << endl;
     
 }
